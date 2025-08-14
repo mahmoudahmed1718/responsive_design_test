9c2dbb2045d41b3eff156fb4fa905cba8860bab8
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-08-14 07:23:07 +0300

    finsh mobil body

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,20 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+      theme: ThemeData(
+        // Material 3 mode
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.deepPurple, // Force AppBar color
+          foregroundColor: Colors.white, // Title/Icon color
+        ),
+      ),
+    );
   }
 }
--- lib/view/widgets/mobil_body.dart
+++ lib/view/widgets/mobil_body.dart
@@ -4,6 +4,34 @@ class MobilBody extends StatelessWidget {
   const MobilBody({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.red);
+    return Scaffold(
+      appBar: AppBar(title: Text("M O B I L B O D Y")),
+
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: AspectRatio(
+              aspectRatio: 16 / 9,
+              child: Container(color: Colors.deepPurple),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 8,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 80,
+                    color: Colors.deepPurple.withValues(alpha: 0.7),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
