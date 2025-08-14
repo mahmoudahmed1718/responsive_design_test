2af2c3bb976d7f02a4fb59e006bf60b60596fa68
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-08-14 06:58:45 +0300

    add layout builder widget to mobil body

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_design/view/widgets/desktop_body.dart';
+import 'package:responsive_design/view/widgets/mobil_body.dart';
 import 'package:responsive_design/view/widgets/responsive_layout.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: ResponsiveLayout(
+        mobileLayout: MobilBody(),
+        desktopLayout: DesktopBody(),
+      ),
+    );
   }
 }
