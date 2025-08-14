269f6de5a7be164a3e44115790b5ac2dfa3aa669
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-08-14 06:54:03 +0300

    add desktop body file

---
+++ lib/view/widgets/desktop_body.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class DesktopBody extends StatelessWidget {
+  const DesktopBody({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(backgroundColor: Colors.green);
+  }
+}
