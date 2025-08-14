ed13230431d4f0c46eba90d28f9ed20ee99c769c
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-08-14 07:36:02 +0300

    finsh also desktop body file

--- lib/view/widgets/desktop_body.dart
+++ lib/view/widgets/desktop_body.dart
@@ -4,6 +4,50 @@ class DesktopBody extends StatelessWidget {
   const DesktopBody({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.green);
+    return Scaffold(
+      appBar: AppBar(title: Text("D E S K T O P B O D Y")),
+      body: Row(
+        children: [
+          Expanded(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: AspectRatio(
+                    aspectRatio: 16 / 9,
+                    child: Container(
+                      height: 250,
+                      width: 400,
+                      color: Colors.deepPurple,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: 8,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          height: 80,
+                          color: Colors.deepPurple.withValues(alpha: 0.7),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              width: 100,
+              color: Colors.deepPurple.withValues(alpha: 2.5),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
