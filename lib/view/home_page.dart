97329b3f7d4b890692422e096a647e6829051c44
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-08-14 06:45:58 +0300

    change the color depend on width

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  const HomePage({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: size.width > 500 ? Colors.green : Colors.red,
+
+      body: Center(child: Text(size.width.toString())),
+    );
   }
 }
